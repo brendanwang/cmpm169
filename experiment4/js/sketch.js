9ecea39ab5b50ca7fac68e0c8996c0e438366575
brendanwang <brendanmwang@gmail.com>
2024-02-12 18:14:27 -0800

    added experiment 4

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,183 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Interactive 3D Solar System
+// Author: Brendan Wang
+// Date: 2/12/2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+
+// Some functions were written by ChatGPT
+// --------------- Controls ---------------
+// Left Mouse Click --------- Rotate
+// Right Mouse Click -------- Pan
+// Scroll Wheel ------------- Zoom In/Out
+
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
-// Globals
-let myInstance;
-let canvasContainer;
+let sunRadius = 200;
+let planetDistances = [500, 650, 800, 950, 1500, 1800, 1900, 2050];
+let planetSizes = [10, 15, 20, 17, 50, 40, 30, 30];
+let planetOrbitSpeeds = [0.02, 0.0175, 0.015, 0.025, 0.01, 0.005, 0.002, 0.001];
+let planetAngles = [0, 0, 0, 0, 0, 0, 0, 0];
+let planetColors = [
+  [175, 170, 169],   // Mercury
+  [236, 200, 120],   // Venus
+  [0, 130, 255],   // Earth
+  [247, 112, 43], // Mars
+  [255, 175, 116], // Jupiter
+  [255, 214, 145], // Saturn
+  [167, 247, 255], // Uranus
+  [0, 88, 216]  // Neptune
+];
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+// Array to store explosion particles
+let explosions = [];
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+// Array to store planet trails
+let planetTrails = [];
 
-// setup() function is called once when the program starts
+
+
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(1920, 1080, WEBGL); // Set canvas size to 1920x1080
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // Start with one explosion
+  createExplosion();
+
+  // Initialize planet trails
+  for (let i = 0; i < planetDistances.length; i++) {
+    planetTrails.push([]);
+  }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+function playBackgroundMusic() {
+  // Check if the sound file is loaded successfully
+  if (backgroundMusic.isLoaded()) {
+    // Loop the background music infinitely
+    backgroundMusic.loop();
+  } else {
+    // If the sound file is not loaded, print an error message
+    console.error('Error: Background music could not be loaded.');
+  }
+}
+
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  // Create space background
+  drawSpaceBackground();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+  // Adjust the view angle to avoid stretching
+  let fov = PI / 3; // 60 degrees
+  let cameraZ = height / 2.0 / tan(fov / 2.0);
+  perspective(fov, width / height, cameraZ / 10.0, cameraZ * 10.0);
+
+  orbitControl();
+
+  // Draw sun
+  fill(212, 57, 15);
+  noStroke();
+  sphere(sunRadius);
+
+  // Update and draw explosions
+  updateAndDrawExplosions();
+
+  // Draw planets and their trails
+  for (let i = 0; i < planetDistances.length; i++) {
+    let distance = planetDistances[i];
+    let planetSize = planetSizes[i];
+    let orbitSpeed = planetOrbitSpeeds[i];
+    let angle = planetAngles[i];
+    let planetColor = planetColors[i]; // Get color for current planet
+
+    // Update planet angle
+    angle += orbitSpeed;
+    planetAngles[i] = angle;
+
+    // Calculate planet position
+    let x = distance * cos(angle);
+    let z = distance * sin(angle);
+
+    // Draw planet
+    push();
+    translate(x, 0, z);
+    fill(planetColor[0], planetColor[1], planetColor[2]); // Use RGB values from planetColor array
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    sphere(planetSize);
+
+    // Add point to the trail
+    let trailPoint = createVector(x, 0, z);
+    planetTrails[i].push(trailPoint);
+
+    // Draw trail
+    beginShape();
+    noFill();
+    strokeWeight(2);
+    stroke(planetColor[0], planetColor[1], planetColor[2], 100); // Use RGB values with transparency
+    for (let p of planetTrails[i]) {
+      let trailX = p.x - x; // Calculate position relative to the planet
+      let trailZ = p.z - z;
+      vertex(trailX, 0, trailZ);
+    }
+    endShape();
+    
+    // Limit the length of the trail
+    if (planetTrails[i].length > 100) {
+      planetTrails[i].shift();
+    }
+    
+    pop();
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function updateAndDrawExplosions() {
+  for (let i = explosions.length - 1; i >= 0; i--) {
+    let explosion = explosions[i];
+
+    // Draw explosion particles
+    push();
+    translate(explosion.position.x, explosion.position.y, explosion.position.z);
+    fill(255, random(100, 255), 0); // Random color for explosion
+    noStroke();
+    sphere(5); // Adjust size as needed
+    pop();
+
+    // Decrease explosion lifespan
+    explosion.lifespan--;
+
+    // Remove explosion if it has expired
+    if (explosion.lifespan <= 0) {
+      explosions.splice(i, 1);
+    }
+  }
+
+  // Add new explosions occasionally
+  if (random(1) < 0.01) {
+    createExplosion();
+  }
+}
+
+function createExplosion() {
+  let explosion = {
+    position: createVector(random(-width, width), random(-height, height), random(-width, width)),
+    lifespan: random(50, 150)
+  };
+  explosions.push(explosion);
+}
+
+function drawSpaceBackground() {
+  // Set background to black
+  background(0);
+
+  // Draw stars
+  randomSeed(4); // Ensure consistent randomness
+  for (let i = 0; i < 200; i++) { // Reduced number of stars
+    let x = random(-3 * width, 3 * width); // Increase the range for x-coordinate
+    let y = random(-3 * height, 3 * height); // Increase the range for y-coordinate
+    let z = random(-3 * width, 3 * width); // Increase the range for z-coordinate
+    let size = random(6, 12);
+    push(); // Save the current transformation matrix
+    translate(x, y, z); // Translate to the star's position
+    fill(255);
+    noStroke();
+    ellipse(0, 0, size, size); // Draw 2D ellipse representing the star
+    pop(); // Restore the transformation matrix
+  }
 }
